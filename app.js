18d2aab8f4227bdaa05ba734d2b32a76a0858cc6
raha <shahsavanraha@gmail.com>
2023-06-17 23:18:13 +0330

    is the whole code with css and js

---
+++ app.js
@@ -0,0 +1,15 @@
+var transitionFunctions = ['ease', 'ease-in', 'ease-out', 'ease-in-out', 'linear'];
+
+document.getElementById('BTN').addEventListener('click', function() {
+  var boxes = document.getElementsByClassName('box');
+  
+  for (var i = 0; i < boxes.length; i++) {
+    var box = boxes[i];
+    var transitionFunction = transitionFunctions[i % transitionFunctions.length];
+    if(box.offsetWidth <500){
+      box.style.transitionTimingFunction = transitionFunction;
+      box.style.width = (box.offsetWidth + 100) + 'px';
+    }
+
+  }
+});
